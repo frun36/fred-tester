78743738f31c11097b4d250e482634f4ae2f7585
frun36 <franek.urbanski@gmail.com>
2025-07-17 14:21:41 +0200

    Add program argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,46 @@
 #include <cstring>
+#include <string>
 
-#include "tests/FredTester.h"
 #include "Logger.h"
+#include "argparse/argparse.hpp"
+#include "tests/FredTester.h"
 
 int main(int argc, char** argv) {
-    if (argc >= 2 && strcmp(argv[1], "-v") == 0) {
-        Logger::setDebug(true);
+    argparse::ArgumentParser program("fred-tester");
+
+    program.add_argument("-d", "--debug")
+        .help("Enable debug mode")
+        .default_value(false)
+        .implicit_value(true);
+
+    program.add_argument("mode")
+        .help(
+            "Operation mode: 'run' (single pass of the tester) or 'dim' (run a DIM server accepting commands)"
+        )
+        .choices("run", "dim");
+
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::runtime_error& err) {
+        Logger::error(
+            "FRED_TESTER",
+            "Failed to parse program arguments: {}",
+            err.what()
+        );
+        std::cout << program;
+        std::exit(1);
     }
 
-    tests::FredTester tester;
-    tester.run();
+    if (program.get<bool>("--debug")) {
+        Logger::setDebug(true);
+        Logger::debug("FRED_TESTER", "Debug mode enabled");
+    }
+
+    if (program.get<std::string>("mode") == "dim") {
+        std::cout << "Running as DIM server";
+    } else {
+        std::cout << "Single run";
+        // tests::FredTester tester;
+        // tester.run();
+    }
 }
