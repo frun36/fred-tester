ec09a41c11d97b400d398ba5133fcf084fd793c6
frun36 <franek.urbanski@gmail.com>
2025-11-12 14:56:11 +0100

    Fix bugs in test generation

--- src/tests/Parameters.cpp
+++ src/tests/Parameters.cpp
@@ -28,7 +28,7 @@ CommandTest Parameters::generateTestOk(
         pattern += std::format("([A-Z0-9_]+),({})\\n", utils::FLT);
     }
 
-    auto valueValidator = [&operations](std::smatch match) -> Result<void> {
+    auto valueValidator = [operations](std::smatch match) mutable -> Result<void> {
         for (size_t i = 1; i < match.size() - 1; i += 2) {
             auto it = std::find_if(
                 operations.begin(),
@@ -44,13 +44,15 @@ CommandTest Parameters::generateTestOk(
                     it->value,
                     match[i + 1].str()
                 );
+            it->validation = true;
         }
 
         for (auto& op : operations) {
             if (!op.validation)
                 return err(
-                    "No response returned for parameter {}",
-                    op.validation
+                    "No response returned for parameter {}\n{}",
+                    op.name,
+                    match[0].str()
                 );
         }
 
@@ -88,7 +90,7 @@ CommandTest Parameters::generateTestErr(
         .pattern(".*{}.*", errorMsg)
         .withoutValueValidator()
         .timeout(0.2)
-        .expectOk()
+        .expectError()
         .build();
 }
 
@@ -99,99 +101,95 @@ Parameters::Parameters(utils::Board board) :
             board,
             "NonexistentParamReadFailed",
             {{"TEST_NONEXISTENT", "READ", 0.}},
-            "nonexistent"
+            "not found"
         ),
         generateTestErr(
             board,
             "NonexistentParamWriteFailed",
             {{"TEST_NONEXISTENT", "WRITE", 0.}},
-            "nonexistent"
+            "not found"
         ),
-        // write outside of valid range
+        generateTestErr(
+            board,
+            "WriteOutOfRange",
+            {{"TEST_A", "WRITE", -0x0D}},
+            "outside the valid range"
+        ),
         generateTestOk(
             board,
             "WriteInitialValues",
             {{"TEST_A", "WRITE", 0x0D},
-             {"TEST_B", "WRITE", 0xF0},
-             {"TEST_C", "WRITE", -1},
+             {"TEST_B", "WRITE", -1},
+             {"TEST_C", "WRITE", 0xF0},
              {"TEST_D", "WRITE", 0xC0FFEE}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_AB",
-            {{"TEST_A", "READ", 0x0D}, {"TEST_B", "READ", 0xF0}}
+            {{"TEST_A", "READ", 0x0D}, {"TEST_B", "READ", -1}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_AllParams",
             {{"TEST_READONLY", "READ", 0xA11CEF17},
-             {"TEST_C", "READ", -1},
-             {"TEST_B", "READ", 0xF0},
-             {"TEST_D", "READ", 0xC0FFEE},
-             {"TEST_A", "READ", 0x0D}}
-        ),
-        generateTestOk(
-            board,
-            "SuccessfulMultiRead_AllParams",
-            {{"TEST_READONLY", "READ", 0xA11CEF17},
-             {"TEST_C", "READ", -1},
-             {"TEST_B", "READ", 0xF0},
+             {"TEST_C", "READ", 0xF0},
+             {"TEST_B", "READ", -1},
              {"TEST_D", "READ", 0xC0FFEE},
              {"TEST_A", "READ", 0x0D}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_AB",
-            {{"TEST_A", "READ", 0x0D}, {"TEST_B", "READ", 0xF0}}
+            {{"TEST_A", "READ", 0x0D}, {"TEST_B", "READ", -1}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_AC",
-            {{"TEST_A", "READ", 0x0D}, {"TEST_C", "READ", -1}}
+            {{"TEST_A", "READ", 0x0D}, {"TEST_C", "READ", 0xF0}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_BD",
-            {{"TEST_B", "READ", 0xF0}, {"TEST_D", "READ", 0xC0FFEE}}
+            {{"TEST_B", "READ", -1}, {"TEST_D", "READ", 0xC0FFEE}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_CReadonly",
-            {{"TEST_READONLY", "READ", 0xA11CEF17}, {"TEST_C", "READ", -1}}
+            {{"TEST_READONLY", "READ", 0xA11CEF17}, {"TEST_C", "READ", 0xF0}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_ABC",
             {{"TEST_A", "READ", 0x0D},
-             {"TEST_B", "READ", 0xF0},
-             {"TEST_C", "READ", -1}}
+             {"TEST_B", "READ", -1},
+             {"TEST_C", "READ", 0xF0}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_BCD",
-            {{"TEST_B", "READ", 0xF0},
-             {"TEST_C", "READ", -1},
+            {{"TEST_B", "READ", -1},
+             {"TEST_C", "READ", 0xF0},
              {"TEST_D", "READ", 0xC0FFEE}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_CDR",
             {{"TEST_READONLY", "READ", 0xA11CEF17},
-             {"TEST_C", "READ", -1},
+             {"TEST_C", "READ", 0xF0},
              {"TEST_D", "READ", 0xC0FFEE}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_ABD",
             {{"TEST_A", "READ", 0x0D},
-             {"TEST_B", "READ", 0xF0},
+             {"TEST_B", "READ", -1},
              {"TEST_D", "READ", 0xC0FFEE}}
         ),
         generateTestOk(
             board,
             "SuccessfulMultiRead_ACD",
             {{"TEST_A", "READ", 0x0D},
-             {"TEST_C", "READ", -1},
+             {"TEST_C", "READ", 0xF0},
              {"TEST_D", "READ", 0xC0FFEE}}
         ),
         generateTestOk(
@@ -202,7 +200,7 @@ Parameters::Parameters(utils::Board board) :
         generateTestOk(
             board,
             "SuccessfulMultiWrite_AC",
-            {{"TEST_A", "WRITE", 0x0D}, {"TEST_C", "WRITE", -32}}
+            {{"TEST_A", "WRITE", 0x0D}, {"TEST_C", "WRITE", 12}}
         ),
         generateTestOk(
             board,
